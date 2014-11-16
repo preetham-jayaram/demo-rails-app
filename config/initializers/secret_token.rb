fdf309bddbf3945745c5a2100c584b186532eab3
your name <your infibeam email>
2014-11-16 12:44:08 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = 'aabcc010dbe3a9d0a65be3a640173d0928a1b211a94ac0ae4fe8fc0b287f420e0073d1ac146eda45f840904243df53169771b6a4cce36d19f3e3771316efb64e'
